10a0909e41ed3cd3a87ff9dce64e3ab16eafa3ec
igrekus <upload234@mail.ru>
2021-08-19 17:52:57 +0300

    wip: implement plots for live measurements

--- primaryplotwidget.py
+++ primaryplotwidget.py
@@ -32,13 +32,17 @@ class PrimaryPlotWidget(QWidget):
         self._grid.addWidget(self._win, 1, 0)
 
         self._plot_00 = self._win.addPlot(row=0, col=0)
+        self._plot_01 = self._win.addPlot(row=0, col=1)
         self._plot_10 = self._win.addPlot(row=1, col=0)
+        self._plot_11 = self._win.addPlot(row=1, col=1)
 
         self._curves_00 = dict()
+        self._curves_01 = dict()
         self._curves_10 = dict()
+        self._curves_11 = dict()
 
-        self._plot_00.setLabel('left', 'Pвых, дБм', **self.label_style)
-        self._plot_00.setLabel('bottom', 'Fмод, МГц', **self.label_style)
+        self._plot_00.setLabel('left', 'Fвых, МГц', **self.label_style)
+        self._plot_00.setLabel('bottom', 'Uупр, В', **self.label_style)
         self._plot_00.enableAutoRange('x')
         self._plot_00.enableAutoRange('y')
         self._plot_00.showGrid(x=True, y=True)
@@ -51,8 +55,22 @@ class PrimaryPlotWidget(QWidget):
         self._plot_00.addItem(self._hLine_00, ignoreBounds=True)
         self._proxy_00 = pg.SignalProxy(self._plot_00.scene().sigMouseMoved, rateLimit=60, slot=self.mouseMoved_00)
 
-        self._plot_10.setLabel('left', 'Iпот, мА', **self.label_style)
-        self._plot_10.setLabel('bottom', 'Uпит, В', **self.label_style)
+        self._plot_01.setLabel('left', 'Pвых, дБм', **self.label_style)
+        self._plot_01.setLabel('bottom', 'Uупр, В', **self.label_style)
+        self._plot_01.enableAutoRange('x')
+        self._plot_01.enableAutoRange('y')
+        self._plot_01.showGrid(x=True, y=True)
+        self._vb_01 = self._plot_01.vb
+        rect = self._vb_01.viewRect()
+        self._plot_01.addLegend(offset=(rect.x() + rect.width() - 50, rect.y() + 30))
+        self._vLine_01 = pg.InfiniteLine(angle=90, movable=False)
+        self._hLine_01 = pg.InfiniteLine(angle=0, movable=False)
+        self._plot_01.addItem(self._vLine_01, ignoreBounds=True)
+        self._plot_01.addItem(self._hLine_01, ignoreBounds=True)
+        self._proxy_01 = pg.SignalProxy(self._plot_01.scene().sigMouseMoved, rateLimit=60, slot=self.mouseMoved_01)
+
+        self._plot_10.setLabel('left', 'P гарм 2 отн', **self.label_style)
+        self._plot_10.setLabel('bottom', 'Uупр, В', **self.label_style)
         self._plot_10.enableAutoRange('x')
         self._plot_10.enableAutoRange('y')
         self._plot_10.showGrid(x=True, y=True)
@@ -64,6 +82,20 @@ class PrimaryPlotWidget(QWidget):
         self._plot_10.addItem(self._vLine_10, ignoreBounds=True)
         self._plot_10.addItem(self._hLine_10, ignoreBounds=True)
         self._proxy_10 = pg.SignalProxy(self._plot_10.scene().sigMouseMoved, rateLimit=60, slot=self.mouseMoved_10)
+
+        self._plot_11.setLabel('left', 'P гарм 3 отн', **self.label_style)
+        self._plot_11.setLabel('bottom', 'Uупр, В', **self.label_style)
+        self._plot_11.enableAutoRange('x')
+        self._plot_11.enableAutoRange('y')
+        self._plot_11.showGrid(x=True, y=True)
+        self._vb_11 = self._plot_11.vb
+        rect = self._vb_11.viewRect()
+        self._plot_11.addLegend(offset=(rect.x() + 50, rect.y() + rect.height() - 30))
+        self._vLine_11 = pg.InfiniteLine(angle=90, movable=False)
+        self._hLine_11 = pg.InfiniteLine(angle=0, movable=False)
+        self._plot_11.addItem(self._vLine_11, ignoreBounds=True)
+        self._plot_11.addItem(self._hLine_11, ignoreBounds=True)
+        self._proxy_11 = pg.SignalProxy(self._plot_11.scene().sigMouseMoved, rateLimit=60, slot=self.mouseMoved_11)
 
         self.setLayout(self._grid)
 
@@ -83,6 +115,22 @@ class PrimaryPlotWidget(QWidget):
                 for p, curve in self._curves_00.items()
             ]))
 
+    def mouseMoved_01(self, event):
+        pos = event[0]
+        if self._plot_01.sceneBoundingRect().contains(pos):
+            mouse_point = self._vb_01.mapSceneToView(pos)
+            x = mouse_point.x()
+            y = mouse_point.y()
+            self._vLine_01.setPos(x)
+            self._hLine_01.setPos(y)
+            if not self._curves_01:
+                return
+
+            self._stat_label.setText(_label_text(x, y, [
+                [p, curve.yData[_find_value_index(curve.xData, x)]]
+                for p, curve in self._curves_01.items()
+            ]))
+
     def mouseMoved_10(self, event):
         pos = event[0]
         if self._plot_10.sceneBoundingRect().contains(pos):
@@ -99,21 +147,43 @@ class PrimaryPlotWidget(QWidget):
                 for p, curve in self._curves_10.items()
             ]))
 
+    def mouseMoved_11(self, event):
+        pos = event[0]
+        if self._plot_11.sceneBoundingRect().contains(pos):
+            mouse_point = self._vb_11.mapSceneToView(pos)
+            x = mouse_point.x()
+            y = mouse_point.y()
+            self._vLine_11.setPos(x)
+            self._hLine_11.setPos(y)
+            if not self._curves_11:
+                return
+
+            self._stat_label.setText(_label_text(x, y, [
+                [p, curve.yData[_find_value_index(curve.xData, x)]]
+                for p, curve in self._curves_11.items()
+            ]))
+
     def clear(self):
         def _remove_curves(plot, curve_dict):
             for _, curve in curve_dict.items():
                 plot.removeItem(curve)
 
         _remove_curves(self._plot_00, self._curves_00)
+        _remove_curves(self._plot_01, self._curves_01)
         _remove_curves(self._plot_10, self._curves_10)
+        _remove_curves(self._plot_11, self._curves_11)
 
         self._curves_00.clear()
+        self._curves_01.clear()
         self._curves_10.clear()
+        self._curves_11.clear()
 
     def plot(self):
         print('plotting primary stats')
-        _plot_curves(self._controller.result.data1, self._curves_00, self._plot_00, prefix='Fгет= ', suffix=' ГГц')
-        _plot_curves(self._controller.result.data2, self._curves_10, self._plot_10, prefix='', suffix='')
+        _plot_curves(self._controller.result.data1, self._curves_00, self._plot_00, prefix='Uпит= ', suffix=' В')
+        _plot_curves(self._controller.result.data2, self._curves_01, self._plot_01, prefix='Uпит= ', suffix=' В')
+        _plot_curves(self._controller.result.data3, self._curves_10, self._plot_10, prefix='', suffix='')
+        _plot_curves(self._controller.result.data4, self._curves_11, self._plot_11, prefix='', suffix='')
 
 
 def _plot_curves(datas, curves, plot, prefix='', suffix=''):
